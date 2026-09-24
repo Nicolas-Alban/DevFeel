a46c212ce5338a1037aa0bb34ecef8a94117919e
Cesar Alban <cesar.alban126@gmail.com>
2026-09-24 11:14:16 -0500

    Conexion con SupaBase

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { loadShared, saveShared } from './storage'
+
+window.__supabaseStorage = { loadShared, saveShared }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
